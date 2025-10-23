c3f42a2983a76df4d3430f8224163f6dd4f6a7c3
Rati Jebisashvili <rjebisashvili@gmail.com>
2025-10-23 07:14:38 +0400

    remove migration

--- db/migrate/20251023001429_implement_soft_delete.rb
+++
@@ -1,9 +0,0 @@
-class ImplementSoftDelete < ActiveRecord::Migration[8.0]
-  def change
-    add_column :words, :deleted_at, :datetime
-    add_index :words, :deleted_at
-
-    add_column :other_qualifications, :deleted_at, :datetime
-    add_index :other_qualifications, :deleted_at
-  end
-end
